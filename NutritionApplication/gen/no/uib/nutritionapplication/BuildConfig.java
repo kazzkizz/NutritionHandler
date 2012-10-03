e1b436320394c07804f23562d155d7e44fc760a9
Ushaka <mariuslaiho@gmail.com>
2012-10-03 15:49:14 +0200

    Eg har gjort jævla mykje!

--- NutritionApplication/gen/no/uib/nutritionapplication/BuildConfig.java
+++ NutritionApplication/gen/no/uib/nutritionapplication/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.example.nutritionapplication;
+package no.uib.nutritionapplication;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
