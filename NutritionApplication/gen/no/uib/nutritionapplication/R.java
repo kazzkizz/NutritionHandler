0582adc9f709114cb0f615aff6c541e992cd5a79
Helen Sørheim <Helen Srheim@129.177.144.43>
2012-10-08 13:17:40 +0200

    weagf

--- NutritionApplication/gen/no/uib/nutritionapplication/R.java
+++ NutritionApplication/gen/no/uib/nutritionapplication/R.java
@@ -15,17 +15,22 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
+        public static final int EditText01=0x7f070003;
+        public static final int LinearLayout01=0x7f070002;
+        public static final int ListView01=0x7f070004;
         public static final int enter_age=0x7f070001;
         public static final int enter_name=0x7f070000;
-        public static final int menu_settings=0x7f070002;
+        public static final int menu_settings=0x7f070005;
     }
     public static final class layout {
         public static final int activity_display_message=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int activity_search=0x7f030002;
     }
     public static final class menu {
         public static final int activity_display_message=0x7f060000;
         public static final int activity_main=0x7f060001;
+        public static final int activity_search=0x7f060002;
     }
     public static final class string {
         public static final int add_meal=0x7f040006;
@@ -33,9 +38,11 @@ public final class R {
         public static final int button_send=0x7f040004;
         public static final int enter_age=0x7f040007;
         public static final int enter_name=0x7f040002;
+        public static final int hello_world=0x7f040008;
         public static final int menu_settings=0x7f040001;
         public static final int title_activity_display_message=0x7f040005;
         public static final int title_activity_main=0x7f040003;
+        public static final int title_activity_search=0x7f040009;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
