95113de4ebae6012210a0708833ab387c531cecc
Ushaka <mariuslaiho@gmail.com>
2012-10-09 15:30:55 +0200

    Prøver og prøver

--- NutritionApplication/gen/no/uib/nutritionapplication/R.java
+++ NutritionApplication/gen/no/uib/nutritionapplication/R.java
@@ -16,8 +16,10 @@ public final class R {
     }
     public static final class id {
         public static final int enter_age=0x7f070001;
+        public static final int enter_height=0x7f070002;
         public static final int enter_name=0x7f070000;
-        public static final int menu_settings=0x7f070002;
+        public static final int enter_weight=0x7f070003;
+        public static final int menu_settings=0x7f070004;
     }
     public static final class layout {
         public static final int activity_display_message=0x7f030000;
@@ -28,14 +30,16 @@ public final class R {
         public static final int activity_main=0x7f060001;
     }
     public static final class string {
-        public static final int add_meal=0x7f040006;
+        public static final int add_meal=0x7f040005;
         public static final int app_name=0x7f040000;
-        public static final int button_send=0x7f040004;
+        public static final int button_send=0x7f040003;
         public static final int enter_age=0x7f040007;
-        public static final int enter_name=0x7f040002;
+        public static final int enter_height=0x7f040008;
+        public static final int enter_name=0x7f040006;
+        public static final int enter_weight=0x7f040009;
         public static final int menu_settings=0x7f040001;
-        public static final int title_activity_display_message=0x7f040005;
-        public static final int title_activity_main=0x7f040003;
+        public static final int title_activity_display_message=0x7f040004;
+        public static final int title_activity_main=0x7f040002;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
--- NutritionApplication/src/no/uib/nutritionapplication/NutritionHandler.java
+++ NutritionApplication/src/no/uib/nutritionapplication/NutritionHandler.java
@@ -26,9 +26,7 @@ public class NutritionHandler extends Activity {
 			
 			public void onClick(View v) {
 				// TODO Auto-generated method stub}
-				  });*/
-
-        
+				  });*/   
     }
     
     @Override
