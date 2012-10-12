e194bdeb0418d28deb334a27164b7ef4adde276e
Kristian Vårdal <kristian.vardal@gmail.com>
2012-10-12 12:01:48 +0200

    removed some small things in acitivitylevel

--- nutritionApplication/src/no/uib/nutritionapplication/ActivityLevel.java
+++ nutritionApplication/src/no/uib/nutritionapplication/ActivityLevel.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
-import android.view.View;
-import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
