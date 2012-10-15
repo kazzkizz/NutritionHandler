a934d5b6c9133450fcee628d19cc3d7070545de1
Kristian Vårdal <kristian.vardal@gmail.com>
2012-10-15 13:49:52 +0200

    Changed nutritionhandler and activitylevel .java.

--- NutritionApplication/src/no/uib/nutritionapplication/ActivityLevel.java
+++ NutritionApplication/src/no/uib/nutritionapplication/ActivityLevel.java
@@ -11,59 +11,49 @@ import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
 
 public class ActivityLevel extends Activity {
-	
-	
-	
+	private RadioGroup radiogroup;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_activity_level);
-        
-        
-        //--- Radiogroup listener
-        RadioGroup radiogroup = (RadioGroup) findViewById(R.id.radioGroup);
-        Intent i = new Intent();
-        radiogroup.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-			
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_activity_level);
+
+
+		//--- Radiogroup listener
+		radiogroup = (RadioGroup) findViewById(R.id.radioGroup);
+		final Intent intent = new Intent();
+		radiogroup.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
 			public void onCheckedChanged(RadioGroup group, int checkedId) {
-        		RadioButton radioBtn1 = (RadioButton) findViewById(R.id.radioLowIntensity);
-        		RadioButton radioBtn2 = (RadioButton) findViewById(R.id.radioMediumIntensity);
-        		RadioButton radioBtn3 = (RadioButton) findViewById(R.id.radioHighIntensity);
-        		
-        		if(radioBtn1.isChecked()) {
-        			//do something         	
-        			System.out.println("hello");
-        		}
-        		
-        		if(radioBtn2.isChecked()) {
-        			//do something
-        			System.out.println("hello2");
-        		}
-        		
-        		if(radioBtn3.isChecked()) {
-        			//do something
-        			System.out.println("hello3");
-        		}
+				RadioButton radioBtn1 = (RadioButton) findViewById(R.id.radioLowIntensity);
+				RadioButton radioBtn2 = (RadioButton) findViewById(R.id.radioMediumIntensity);
+				RadioButton radioBtn3 = (RadioButton) findViewById(R.id.radioHighIntensity);
+
+				group.addView(radioBtn1);
+				group.addView(radioBtn2);
+				group.addView(radioBtn3);
+				//				group.getFocusedChild();
 			}
 		});
-        
-        //--Next-button listener
-        Button nextButton = (Button) findViewById(R.id.btn_activityNext);
-        nextButton.setOnClickListener(new View.OnClickListener() {
-        	
-        	public void onClick(View v) {
-        		//do something
-        		System.out.println("next-button-pressed");
-        	}
-        });
-    }
-    
-    
+		
+		Button nextButton = (Button) findViewById(R.id.btn_activityNext);
+		nextButton.setOnClickListener(new View.OnClickListener() {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_activity_level, menu);
-        return true;
-    }
+
+			public void onClick(View v) {
+				RadioButton b = (RadioButton) radiogroup.getFocusedChild();
+				b.getText();
+				System.out.println(b.getText());
+				
+			}
+		});
+	}
+
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_activity_level, menu);
+		return true;
+	}
 }
