d608b1992be9726b91f7fa2fb8bab92f7c6bf2c0
Kristian Vårdal <kristian.vardal@gmail.com>
2012-10-18 15:07:55 +0200

    some work on displaymessage and nutritionhandler and so on. pushed to developer because it has bugs :)

--- NutritionApplication/src/no/uib/nutritionapplication/DisplayMessageActivity.java
+++ NutritionApplication/src/no/uib/nutritionapplication/DisplayMessageActivity.java
@@ -9,27 +9,61 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends Activity {
+	
+	private String name;
+	private int age;
+	private int height;
+	private int weight;
+	private String gender;
+	
+	private TextView nameTextView; 
+    private TextView ageTextView;
+    private TextView heightTextView;
+    private TextView weightTextView;
+    private TextView genderTextView;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
-
-	    // Get the message from the intent
-	    Intent intent = getIntent();
 	    
-	    //Used to get data from the value-pair.
-	    Bundle b = intent.getExtras();
-	    String n = (String) b.get("name");
-
-	    // Create the text view
-	    TextView textView = new TextView(this);
-	    textView.setTextSize(40);
-	    textView.setText(n);
-
-	    // Set the text view as the activity layout
-	    setContentView(textView);
+	    /** Retrieve the data from the intent and add them to textview's  **/
+	    
+	    setContentView(R.layout.activity_display_message);
+	    Bundle userData = getIntent().getExtras(); 
+	    
+	    if(userData != null) {
+	    	this.name = userData.getString("name");
+	    	this.age = userData.getInt("age");
+//	    	height = userData.getInt("height");
+//	    	weight = userData.getInt("weight");
+//	    	gender = userData.getString("gender");
+	    	
+	    	this.nameTextView = (TextView) findViewById(R.id.nameText);
+	        this.ageTextView = (TextView) findViewById(R.id.ageText);
+	        this.heightTextView = (TextView) findViewById(R.id.heightText);
+	        this.weightTextView = (TextView) findViewById(R.id.weightText);
+	        this.genderTextView = (TextView) findViewById(R.id.genderText);
+	    
+		    this.nameTextView.setText(this.name);
+		    this.ageTextView.setText(this.age);
+		    
+	    }
 	}
 
+//    // Get the message from the intent
+//    Intent intent = getIntent();
+//    
+//    //Used to get data from the value-pair.
+//    Bundle bundle = intent.getExtras();
+//    String name = (String) bundle.get("name");
+    
+//    // Create the text view
+//    TextView textView = new TextView(this);
+//    textView.setTextSize(40);
+//    textView.setText(name);
+
+    // Set the text view as the activity layout
+//    setContentView(nameTextView);
 	   
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- NutritionApplication/src/no/uib/nutritionapplication/NutritionHandler.java
+++ NutritionApplication/src/no/uib/nutritionapplication/NutritionHandler.java
@@ -23,25 +23,28 @@ public class NutritionHandler extends Activity {
 
     public void sendMessage(View view) {
     	 Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	 
+    	 /** EditText-field for personal information **/
+    	 
          EditText editName = (EditText) findViewById(R.id.enter_name);
          String nameString = editName.getText().toString();
          intent.putExtra("name", nameString);
         
          EditText editAge = (EditText) findViewById(R.id.enter_age);
-         String ageString= editAge.getText().toString();
-         intent.putExtra("age", ageString);
-
-         EditText editHeight = (EditText) findViewById(R.id.enter_height);
-         String heightString = editHeight.getText().toString();
-         intent.putExtra("height", heightString);
-
-         EditText editWeight = (EditText) findViewById(R.id.enter_weight);
-         String weightString = editWeight.getText().toString();
-         intent.putExtra("weight", weightString);
-
-         EditText editGender = (EditText) findViewById(R.id.enter_gender);
-         String genderString= editGender.getText().toString();
-         intent.putExtra("gender", genderString);
+         int ageNumber= Integer.parseInt(editAge.getText().toString());
+         intent.putExtra("age", ageNumber);
+//
+//         EditText editHeight = (EditText) findViewById(R.id.enter_height);
+//         int heightNumber= Integer.parseInt(editHeight.getText().toString());
+//         intent.putExtra("height", heightNumber);
+//
+//         EditText editWeight = (EditText) findViewById(R.id.enter_weight);
+//         int weightNumber= Integer.parseInt(editWeight.getText().toString());
+//         intent.putExtra("weight", weightNumber);
+//
+//         EditText editGender = (EditText) findViewById(R.id.enter_gender);
+//         String genderString= editGender.getText().toString();
+//         intent.putExtra("gender", genderString);
 
          startActivity(intent);
      }
